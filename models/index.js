530a932c36c32b7165299b0127431ef7b53d8395
Allison French <alliefrench22@gmail.com>
2019-03-07 17:18:05 -0500

    adding hook to handle slug

--- models/index.js
+++ models/index.js
@@ -1,9 +1,15 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/wikistack');
 
+function generateSlug (title) {
+  // Removes all non-alphanumeric characters from title
+  // And make whitespace underscore
+  return title.replace(/\s+/g, '_').replace(/\W/g, '');
+}
+
 const Page = db.define('page', {
   title: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
   },
   slug: {
     type: Sequelize.STRING
@@ -15,6 +21,10 @@ const Page = db.define('page', {
     type: Sequelize.ENUM('open', 'closed')
   }
 });
+
+Page.beforeCreate((pageInstance) => {
+  pageInstance.slug = generateSlug(pageInstance.title)
+})
 
 const User = db.define('user', {
   name: {
