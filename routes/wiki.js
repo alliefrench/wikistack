ab8c61ccfa2673a6d1be706b5a8fc1d255b3b6d4
Nancy <mitcheaven@gmail.com>
2019-03-07 17:06:47 -0500

    added post action

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,16 +1,41 @@
 const express = require("express");
 const router = express.Router();
+//const addPage = require('../views/addPage.js')
+//console.log(addPage)
+const { Page } = require("../models")
+// const { addPage } = require("../views");
+// console.log(addPage)
+const addPage = require("../views/addPage.js");
+//console.log(addPage)
 
 router.get('/', (req, res, next) => {
     res.send('hello!')
 })
 
-router.post('/', (req, res, next) => {
-    '???'
+router.post('/', async (req, res, next) => {
+    //console.log(req.body)
+    let contentObj = req.body
+    title = contentObj.title
+    content = contentObj.content
+    console.log(content)
+    // console.log(contentObj)
+
+    const page = new Page({
+        title: title,
+        content: content
+      });
+
+
+      try {
+        await page.save();
+        res.redirect('/');
+      } catch (error) { next(error) }
 })
 
 router.get('/add', (req, res, next) => {
-    res.send('hello again!')
+    res.send(
+        addPage()
+    )
 })
 
 module.exports = router
